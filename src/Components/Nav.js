dbb1dfb44f1b04ec2dee77d91649ecc757e8daea
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-02 21:58:15 +0530

    nav icon added

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,6 +1,11 @@
 import React from "react";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 import NavCss from "./CSS/Nav.module.css";
 
 export default function Nav() {
-  return <div className={NavCss.mDivN}>Nav</div>;
+  return (
+    <div className={NavCss.mDivN}>
+      <PlayCircleFilledIcon />
+    </div>
+  );
 }
