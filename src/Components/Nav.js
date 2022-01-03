f2384b19c38bd6c402275396e9cfdc4ed15bdd0e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-03 13:44:27 +0530

    link added

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,14 +1,17 @@
 import React from "react";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 import NavCss from "./CSS/Nav.module.css";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
     <div className={NavCss.mDivN}>
-      <div className={NavCss.iconDiv}>
-        <PlayCircleFilledIcon />
-        <p>Video </p>
-      </div>
+      <Link to="/" className={NavCss.LinkH}>
+        <div className={NavCss.iconDiv}>
+          <PlayCircleFilledIcon />
+          <p>Video </p>
+        </div>
+      </Link>
     </div>
   );
 }
